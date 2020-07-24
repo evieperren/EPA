4f26ab419073a518bc1b4fd95bc1e42b01623501
Evie Perren <evie.perren@simplyhealth.co.uk>
2020-07-24 11:04:21 +0100

    creating readme, correcting spelling

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const server = require('./index')
 mongoose.connect('mongodb://localhost:4300/first-catering', {useNewUrlParser: true, useUnifiedTopology: true , useCreateIndex: true})
 .then(() => {
   console.log('Successfully connected to database')
-  const setup = server.listen(4301, () => console.log(`Listerning on port: 4301`))
+  const setup = server.listen(4301, () => console.log(`Listening on port: 4301`))
   app.use('/api/v1', require('./router/router'))
   
   setTimeout(() => {
